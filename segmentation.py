ea6f494bde84eed7e219e5801a83fb957aaa5815
Maria Jaramillo <mi.jaramillo@uniandes.edu.co>
2020-04-17 15:49:05 -0500

    dice function added

--- segmentation.py
+++ segmentation.py
@@ -306,3 +306,41 @@ print ('Dice similarity score is: ' + format(dice))
 # 13. Establecer un baseline y generar resultados (experimentos) a partir de eso
 
 ################################
+
+def dice(im1, im2):
+    """
+     TOMADO DE: https://gist.github.com/JDWarner/6730747
+    Computes the Dice coefficient, a measure of set similarity.
+    Parameters
+    ----------
+    im1 : array-like, bool
+        Any array of arbitrary size. If not boolean, will be converted.
+    im2 : array-like, bool
+        Any other array of identical size. If not boolean, will be converted.
+    Returns
+    -------
+    dice : float
+        Dice coefficient as a float on range [0,1].
+        Maximum similarity = 1
+        No similarity = 0
+    Notes
+    -----
+    The order of inputs for `dice` is irrelevant. The result will be
+    identical if `im1` and `im2` are switched.
+    """
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+
+    return 2. * intersection.sum() / (im1.sum() + im2.sum())
+
+
+# DICE TOTAL
+dice_result = dice(y_test,preds_test_t)
+print ('Dice coefficient is: ' + format(dice_result))
+
